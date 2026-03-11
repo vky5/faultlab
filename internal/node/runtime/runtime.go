e10e523e1891296452564d1e47b83c680eceb909
vky5 <vky05@proton.me>
2026-03-11 11:38:11 +0530

    added implementation for stopping nodes

--- internal/node/runtime/runtime.go
+++ internal/node/runtime/runtime.go
@@ -2,6 +2,7 @@ package runtime
 
 import (
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/vky5/faultlab/internal/node"
@@ -13,7 +14,6 @@ type Runtime struct {
 	server *grpc.Server
 	peersMu sync.RWMutex // needed so we don't update peers and ping the peer list at the same time
 } 
-
 // I get it now. Mutex is just a lock and we mentally think where we need and want to use it.
 // it doesnt need to linked to a variable to which we need to read or write. this could be handelled by ourself
 
@@ -27,7 +27,7 @@ func New(cfg node.NodeConfig) Runtime {
 func (r *Runtime) Start() {
 	fmt.Printf("starting node %s on port %d\n", r.config.ID, r.config.Port)
 
-	r.server = node.NewServer(r.config)
+	r.server = node.NewServer(r)
 	go node.StartGRPCServer(r.config, r.server)
 
 	go r.startPingLoop()
@@ -36,11 +36,15 @@ func (r *Runtime) Start() {
 	select {}
 }
 
-// to stop the RPC server
+// to stop the RPC server and node
 func (r *Runtime) Stop() {
 	if r.server == nil {
 		return
 	}
 
 	r.server.GracefulStop()
+
+	os.Exit(0) // exit the process after stopping the server 
+
+	// ! we are exiting the process but defer will not run so be careful, create a separate func to handle cleanup
 }
--- internal/node/server.go
+++ internal/node/server.go
@@ -10,15 +10,19 @@ import (
 	"google.golang.org/grpc"
 )
 
+type NodeController interface {
+	Stop()
+}
+
 type NodeRPCServer struct {
-	cfg NodeConfig
+	nc NodeController
 	protocol.UnimplementedNodeServiceServer
 }
 
-func NewServer(cfg NodeConfig) *grpc.Server {
+func NewServer(nc NodeController) *grpc.Server {
 	server := grpc.NewServer()
 	protocol.RegisterNodeServiceServer(server, &NodeRPCServer{
-		cfg: cfg,
+		nc: nc,
 	})
 	return server
 
@@ -43,3 +47,9 @@ func (n *NodeRPCServer) Ping(ctx context.Context, _ *protocol.PingRequest) (*pro
 		Message: "Pong",
 	}, nil
 }
+
+
+func (n *NodeRPCServer) StopNode(ctx context.Context, _ *protocol.RemoveNodeRequest) (*protocol.RemoveNodeResponse, error) {
+	n.nc.Stop()
+	return &protocol.RemoveNodeResponse{}, nil
+}	
